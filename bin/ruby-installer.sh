985ae9e2336e60c8bc214f90e62c7ef7761b0711
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-10-10 12:28:55 +0200

    Update ruby-installer.sh

--- bin/ruby-installer.sh
+++ bin/ruby-installer.sh
@@ -4,13 +4,37 @@ export RBENV_ROOT="${OPENSHIFT_DATA_DIR}.rbenv"
 export RBENV_INIT="${OPENSHIFT_DATA_DIR}rbenv-init.sh"
 export RUBY_VERSION="2.0.0-p353"
 
-if [ ! -d "${RBENV_ROOT}" ] ; then
+# Install rbenv:
+echo "Installing rbenv..."
+git clone https://github.com/sstephenson/rbenv.git $RBENV_ROOT
+echo "Done."
 
-    curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+# Install plugins:
+PLUGINS=(
+  sstephenson/ruby-build
+)
 
-    echo "gem: --no-rdoc --no-ri" > ${OPENSHIFT_DATA_DIR}.gemrc
+echo "Installing plugins..."
+for plugin in ${PLUGINS[@]} ; do
 
-    echo '
+  KEY=${plugin%%/*}
+  VALUE=${plugin#*/}
+
+  RBENV_PLUGIN_ROOT="${RBENV_ROOT}/plugins/$VALUE"
+  if [ ! -d "$RBENV_PLUGIN_ROOT" ] ; then
+    git clone https://github.com/$KEY/$VALUE.git $RBENV_PLUGIN_ROOT
+  else
+    cd $RBENV_PLUGIN_ROOT
+    echo "Pulling $VALUE updates."
+    git pull
+  fi
+
+done
+echo "Done."
+
+echo "gem: --no-rdoc --no-ri" > ${OPENSHIFT_DATA_DIR}.gemrc
+
+echo '
 export RBENV_ROOT="${OPENSHIFT_DATA_DIR}.rbenv"
 
 if [ -d "${RBENV_ROOT}" ]; then
@@ -19,12 +43,11 @@ if [ -d "${RBENV_ROOT}" ]; then
 fi
 
 alias gem="gem --config-file ${OPENSHIFT_DATA_DIR}.gemrc"
-    ' > $RBENV_INIT
+' > $RBENV_INIT
 
-    source $RBENV_INIT
+source $RBENV_INIT
 
-    rbenv install $RUBY_VERSION
-    rbenv global $RUBY_VERSION
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
 
-    gem install bundler
-fi
+gem install bundler
